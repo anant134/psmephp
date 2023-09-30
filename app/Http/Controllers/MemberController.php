1911345d32fae973fa8cc96914cdd061e1e3ec69
akshay <anant@ideas2tech.com>
2023-09-30 23:32:31 +0800

    changes

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 use App\Models\Member;
 use App\Models\MemberPersonalInformation;
 use App\Models\RegistrationType;
+
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class MemberController extends BaseController
@@ -30,30 +32,44 @@ class MemberController extends BaseController
     }
     public function getAllMember(Request $request){
         try {
-
-            $queryModel = MemberPersonalInformation::query();
+            $queryModel = DB::table('registration_temp_personal_information')
+            ->leftJoin('registration_temp_professional_credentials', 'registration_temp_professional_credentials.personal_information_id', '=', 'registration_temp_personal_information.personal_information_id')
+            ->leftJoin('registration_temp_psme_membership_verification', 'registration_temp_psme_membership_verification.personal_information_id', '=', 'registration_temp_personal_information.personal_information_id')
+            ->leftJoin('registration_type_of_registration', 'registration_type_of_registration.type_of_registration_id', '=', 'registration_temp_personal_information.type_of_registration')
+            ->leftJoin('registration_type_of_membership', 'registration_type_of_membership.type_of_membership_id', '=', 'registration_temp_psme_membership_verification.type_of_membership')
+            ->leftJoin('registration_psme_chapter', 'registration_psme_chapter.psme_chapter_id', '=', 'registration_temp_psme_membership_verification.psme_chapter')
+           
+            ->select('registration_temp_personal_information.*', 
+            'registration_temp_professional_credentials.*',
+            'registration_temp_psme_membership_verification.*',
+            'registration_type_of_registration.*',
+            'registration_type_of_membership.*',
+            'registration_psme_chapter.*',
+            DB::raw('concat(registration_temp_personal_information.first_name," ",registration_temp_personal_information.middle_name," ",registration_temp_personal_information.last_name) as fullname' ),
+            DB::raw('CASE WHEN  registration_type_of_registration.type_of_registration_id = 3 THEN "11THPMCH-VSTR-"
+                     ELSE CONCAT("71STNC-",
+                     CASE 
+                        WHEN registration_type_of_registration.type_of_registration_id = 1 THEN "DLGT-"  
+                        WHEN registration_type_of_registration.type_of_registration_id = 4 THEN "NBOT-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 5 THEN "CPRS-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 6 THEN "TDCH-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 7 THEN "PSTP-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 8 THEN "CHRP-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 9 THEN "CMMT-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 10 THEN "CMMT-"
+                        WHEN registration_type_of_registration.type_of_registration_id = 11 THEN "SVCP-"
+                     END,  registration_temp_personal_information.personal_information_id)
+                     END AS controlnumber'))
+            ->where('registration_temp_personal_information.status_of_transaction', '=', 'Paid')
+            ->where('registration_temp_personal_information.request_official_receipt', '=', 'true')
+            ->groupby('registration_temp_personal_information.personal_information_id')->distinct()
+            ->orderBy("registration_temp_personal_information.personal_information_id", "desc");
             if ($request->has('type')) {
                 $filter_key = trim($request->get('type'));
-                if($filter_key!=""){
-                    $queryModel = $queryModel->with('registration')
-                    ->where('status_of_transaction', '=', 'Paid')
-                    ->where('type_of_registration', '=', $filter_key)
-                    ->orderBy("personal_information_id", "desc");
-                }else{
-                    $queryModel = $queryModel->with('registration')
-                    ->where('status_of_transaction', '=', 'Paid')
-                    ->orderBy("personal_information_id", "desc");
+                if(!empty( $filter_key)){
+                    $queryModel = $queryModel->where('registration_temp_personal_information.type_of_registration', '=', $filter_key);
                 }
-            
-            }else{
-                $queryModel = $queryModel->with('registration')
-                ->where('status_of_transaction', '=', 'Paid')
-                ->orderBy("personal_information_id", "desc");
             }
-            
-
-
-
             
             $queryModel = $queryModel->get();
             return response()->json(['resultKey' => 1, 'resultValue' => $queryModel, 'errorCode' => null,'errorMsg' => null], 200);
--- app/Models/ProfessionalCredentials.php
+++ app/Models/ProfessionalCredentials.php
@@ -9,16 +9,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\ModelExtender;
 
-class MemberPersonalInformation extends Model 
+class RegistrationType extends Model 
 {
     use SoftDeletes,ModelExtender;
     protected $auto_fillable = ["created_by", "updated_by"];
     protected $table = 'registration_temp_personal_information';
-    protected $guarded = ['id'];
+    protected $guarded = ['professional_credentials_id'];
 
-    public function registration()
-    {
-        return $this->belongsTo(RegistrationType::class, 'type_of_registration', 'type_of_registration_id');
-    }
-    
+  
 }
