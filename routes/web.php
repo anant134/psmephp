eda8755947051c9c5c4310a23b9d0a99e8bc7c6a
akshay <anant@ideas2tech.com>
2023-10-09 15:57:24 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,7 @@ $router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($ro
     });
     $router->group(['prefix'=>'member'],function($router){
         $router->get('getAllMember','MemberController@getAllMember');
+        $router->get('getMemberwithlimit','MemberController@getMemberwithlimit');
         $router->get('getRegistrationType','MemberController@getRegistrationType');
         $router->get('getMemberType','MemberController@getMemberType');
         $router->post('saveMemberType','MemberController@saveMemberType');
